b9d69ae9bd1989ebccfdc077e393561593f4c6e7
Potrijo <backinmarc@gmail.com>
2021-11-30 17:38:54 +0100

    Sentinele

--- Prueba231121.py
+++ Prueba231121.py
@@ -1,6 +1,6 @@
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, PKCS1_v1_5
 from Crypto.PublicKey import RSA
-
+from random import *
 def createPublicKey():
     keyP = RSA.generate(2048)
     with open('myPublickey.pem','wb') as fPublic:
@@ -29,15 +29,23 @@ def EncodeMessage():
     print(str(ciphertext))
     with open('encodedMessage.pem', 'wb') as codedFile:
         codedFile.write(ciphertext)
+    
+    return ciphertext
 
 def DecodeMessage():
     with open('encodedMessage.pem', 'rb') as codedFile:
         encodedMessage = codedFile.read()
         print("\nQuevoyyyyy\n"+str(encodedMessage))
     key = RSA.importKey(open('myPrivatekey.pem').read())
-    cipher = PKCS1_OAEP.new(key)
-    decodedMessage = cipher.decrypt(encodedMessage)
-    print(decodedMessage)
+    cipher = PKCS1_v1_5.new(key)
+    ciphertext = EncodeMessage()
+    sentinel = Random.new().read(256)   
+    print("\Cifraclub\n"+str(ciphertext))
+    decodedMessage = cipher.decrypt(ciphertext, sentinel)
+    
+
+    
+
     
 EncodeMessage()
 DecodeMessage()
